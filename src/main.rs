c6223f4043044e4a0dc73228069a4691cb3c3971
Andrew <ondralukes06@seznam.cz>
2025-04-17 09:39:50 +0200

    extracted inner html

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use reqwest;
 use scraper::{
-    Html, Selector
+    Html, Selector  , ElementRef 
 };
+// use scraper::element_ref
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -14,8 +15,18 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let document: Html = Html::parse_document(&response);
 
     let selector: Selector = Selector::parse("div[class*='datum']").unwrap();
-    let last_element: Option<scraper::ElementRef<'_>> = document.select(&selector).last();    
+    let last_element: Option<ElementRef<'_>> = document.select(&selector).last();    
+    if  last_element.is_none() {
+        println!("No elements found");
+        return Ok(());
+    }
     println!("Last element: {:?}", last_element);
+    if let Some(element) = last_element {
+        let date = element.inner_html();
+        println!("Date: {}", date);
+    }
+    // let date = last_element.html() //last_element.html();  //.value().collect::<Vec<_>>().join(" ");
+    // println!("Date: {}", date);
     // {
     //     let date = element.text().collect::<Vec<_>>().join(" ");
     //     println!("Date: {}", date);
