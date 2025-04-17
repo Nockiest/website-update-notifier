29d5b4c4633561b2bbbf4c143451bf0218ca4845
Andrew <ondralukes06@seznam.cz>
2025-04-17 10:26:29 +0200

    formatted date

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use reqwest;
 use scraper::{
     Html, Selector  , ElementRef 
 };
-use std::fs::File;
-use std::io::Write; // for File
-
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Write}; // for File
+use regex::Regex;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = "https://www.jidelna.cz/jidelni-listek/?jidelna=53";
@@ -14,19 +14,41 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .await?;
 
     let document: Html = Html::parse_document(&response);
-
+    let previous_date = read_previous_date();
+    println!("Previous date: {}", previous_date);
     let selector: Selector = Selector::parse("div[class*='datum']").unwrap();
     let last_element: Option<ElementRef<'_>> = document.select(&selector).last();    
-    // if  last_element.is_none() {
-    //     println!("No elements found");
-    //     return Ok(());
-    // }
     println!("Last element: {:?}", last_element);
     if let Some(element) = last_element {
-        let date: String = element.inner_html();
-        println!("Date: {}", date);
-        let mut file = File::create("last_date.txt").unwrap();
-        file.write_all(date.as_bytes() ).unwrap();
+        let raw_date: String = element.inner_html();
+        println!("new_date: {}", raw_date);
+        
+        let re = Regex::new(r"(\d{1,2})\.\s*(\d{1,2})\.\s*(\d{4})").unwrap();
+        
+        if let Some(captures) = re.captures(&raw_date) {
+            let day = &captures[1];
+            let month = &captures[2];
+            let year = &captures[3];
+            println!("day: {}, month: {}, year: {}", day, month, year);
+            let formatted_date = format!("{}-{}-{}", year, month, day);
+            let mut file = File::create("previous_date.txt").unwrap();
+            file.write_all(formatted_date.as_bytes() ).unwrap();
+        } else {
+            println!("No match found");
+        }
+        
+        
+       
     }
     Ok(())
+}
+
+fn read_previous_date() -> String {
+    let mut contents = String::new();
+    if let Ok(mut file) = File::open("previous_date.txt") {
+        file.read_to_string(&mut contents).unwrap();
+    } else   {
+        return String::new();
+    }
+    contents
 }
